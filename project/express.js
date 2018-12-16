1a8fd4294e836f9a0ed247fa4d679a30bb389005
Tan <tanalbert.02119@gmail.com>
2018-12-16 23:37:11 +0100

    Everything up to 3.2

--- project/express.js
+++ project/express.js
@@ -6,11 +6,11 @@ const bodyParser = require ( 'body-parser' );
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var db = mysql.createConnection({
-	host: "localhost" ,
-	user: "root" ,
-	password: "" ,
-	database: "project" ,
-	port: "3306"
+	host: process.env.MYSQL_HOST,
+	user: process.env.MYSQL_USER,
+	password: process.env.MYSQL_PASSWORD,
+	database: process.env.MYSQL_DATABASE,
+	port: process.env.MYSQL_PORT
 });
 
 //////////////////////////////////////////////////
